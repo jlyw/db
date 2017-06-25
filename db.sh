84d586c1d63a4f660679c0674894da63aa4c7d60
Jean Law Yim Wan <jean.lawyimwan@gmail.com>
2017-06-25 18:27:17 +0200

    Can delete database

--- db.sh
+++ db.sh
@@ -9,6 +9,17 @@ mysql_password=''
 ### Variables
 user_has_set_mysql_password=false
 
+### Test if value is numeric
+function isNumeric {
+	reg='^[0-9]+$'
+
+	if ! [[ $1 =~ $reg ]]; then
+		echo false
+	else
+		echo true
+	fi
+}
+
 ### CONSTRUCT
 function construct {
 	if ! type mysql >/dev/null 2>&1; then
@@ -55,6 +66,7 @@ function chooseAction {
 	echo ""
 	echo "Available actions:"
 	echo "    1/ List available databases"
+	echo "    2/ Delete a database"
 	echo " "
 
 	read -p "Choose an action: " action_number
@@ -63,7 +75,9 @@ function chooseAction {
 	case $action_number in
 		1 )
 			actionListDatabases
-			exit
+			;;
+		2 )
+			actionDeleteDatabase
 			;;
 		* )
 			echo "$(tput setaf 3)WARN : incorrect action$(tput sgr 0)"
@@ -117,13 +131,87 @@ function quitProgram {
 			;;
 		'n' )
 			chooseAction
-			break
 			;;
 		* )
 			echo "$(tput setaf 3)WARN : incorrect answer$(tput sgr 0)"
 			quitProgram
 			;;
 	esac
+}
+
+### CONFIRM DATABASE TO DELETE
+function confirmDatabaseDeletion {
+	read -p "Are you sure you want to delete this database ? (y/n) " confirm_delete_db
+
+	case $confirm_delete_db in
+		n|y )
+			shift
+			;;
+		* )
+			echo "$(tput setaf 3)WARN : incorrect answer$(tput sgr 0)"
+			confirmDatabaseDeletion
+			;;
+	esac
+
+	echo $confirm_delete_db
+}
+
+### DELETE DATABASE
+function actionDeleteDatabase {
+	mysql_db=( $(mysql -u $mysql_user --password=$mysql_password -e "SHOW databases") )
+
+	declare -A db_list
+	i=0
+
+	for db in ${mysql_db[@]}; do
+		if [[ $i == 0 ]]; then
+			((i++))
+		else
+			if ! [[ $db == "information_schema" || $db == "mysql" || $db == "performance_schema" || $db == "sys" ]]; then
+				db_list[$i]=$db
+				((i++))
+			fi
+		fi
+	done
+
+	echo " "
+	echo "Available databases"
+
+	for db_id in ${!db_list[*]}; do
+		echo "    (${db_id}) ${db_list[${db_id}]}"
+	done
+
+	echo " "
+
+	read -p "Database ID to delete: " db_id_to_delete
+
+	echo " "
+
+	while [[ ${db_list[${db_id_to_delete}]} == "" || ($(isNumeric $db_id_to_delete) == false) ]]; do
+		printf "$(tput setaf 3)WARN: incorrect database id.$(tput sgr 0)\n"
+		read -p "Database ID to delete: " db_id_to_delete
+	done
+
+	echo "Database ${db_list[${db_id_to_delete}]} selected"
+	echo " "
+
+	confirm_delete=$(confirmDatabaseDeletion)
+
+	echo " "
+
+	if [[ $confirm_delete == 'n' ]]; then
+		echo "Task canceled"
+		quitProgram
+	fi
+
+	if ! [ `mysql -u $mysql_user --password=$mysql_password -e "DROP database ${db_list["$db_id_to_delete"]}" 2>&1 | grep -v "[Warning]"` ]; then
+		echo "$(tput setaf 2)Database ${db_list["$db_id_to_delete"]} has been deleted!$(tput sgr 0)"
+	else
+		echo "$(tput setaf 1)ERR : cannot delete database ${db_list["$db_id_to_delete"]}.$(tput sgr 0)"
+	fi
+	
+	echo " "
+	quitProgram
 }
 
 construct
